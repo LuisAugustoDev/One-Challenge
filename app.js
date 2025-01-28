4475e54ac4653c0ed66867ff343b2104fe4ccb48
LuisAugustoDev <fluisaugusto603@gmail.com>
2025-01-28 11:22:38 -0300

    Finalização projeto challenge

---
+++ app.js
@@ -0,0 +1,38 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo() {
+    const inputElement = document.getElementById("amigo");
+    const inputText = inputElement.value.trim();
+
+    if (inputText === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+
+    amigos.push(inputText);
+    inputElement.value = "";
+
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    amigos.forEach(amigo => {
+        const li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+            if (amigos.length === 0) {
+                alert("A lista de amigos está vazia.");
+                return;
+            }
+
+            const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+            const amigoSelecionado = amigos[indiceAleatorio];
+
+            const resultado = document.getElementById("resultado");
+            resultado.innerHTML = `Amigo sorteado: <strong>${amigoSelecionado}</strong>`;
+}
